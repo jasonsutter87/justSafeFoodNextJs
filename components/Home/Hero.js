c6e909050efbadf5e82fc92a8af1b87612e6a903
Jason Sutter <jasonsutter87@gmail.com>
2023-11-03 23:20:57 -0700

    updating

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,22 +1,54 @@
 import React from 'react';
+import builder, { BuilderComponent } from '@builder.io/react';
+
+// Define the Builder.io component name for your Hero section
+const HERO_COMPONENT_NAME = 'hero';
+
+// Function to fetch Builder.io content
+const fetchHeroContent = async () => {
+  try {
+    const content = await builder.get('page', { name: HERO_COMPONENT_NAME });
+    return content.data; // You may need to adapt this based on your data structure
+  } catch (error) {
+    console.error('Error fetching content from Builder.io', error);
+    return null;
+  }
+};
 
 const HeroSection = () => {
+  const [heroContent, setHeroContent] = React.useState(null);
+
+  React.useEffect(() => {
+    fetchHeroContent().then((data) => {
+      setHeroContent(data);
+    });
+  }, []);
+
   return (
     <section id="inner-full-size-hero">
       <div className="site-container-1340">
         <div className="row">
           <div className="col-12 col-lg-6 content">
-            <h1>Get Early Access</h1>
-            <h6>We’re launching our early access release this November, if you’re interested in working with us, please don’t hesitate to reach out.</h6>
-
-            <div className="py-4">
-              <a href="/contact" className="btn-primary btn-size-lg">Get Connected</a>
-            </div>
+            {heroContent ? (
+              <>
+                <h1>{heroContent.title}</h1>
+                <h6>{heroContent.subtitle}</h6>
+                <div className="py-4">
+                  <a href={heroContent.buttonLink} className="btn-primary btn-size-lg">
+                    {heroContent.buttonText}
+                  </a>
+                </div>
+              </>
+            ) : (
+              <p>Loading...</p>
+            )}
           </div>
         </div>
       </div>
       <div className="inner-hero-photo">
-        <img src="/images/assets/start-now/justsafefood-start-now-marquee-phase1.webp" alt="Image of a restaurant worker cleaning a table." />
+        {heroContent && (
+          <img src={heroContent.imageSrc} alt={heroContent.imageAlt} />
+        )}
       </div>
     </section>
   );
